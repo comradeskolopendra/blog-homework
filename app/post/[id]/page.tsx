27c888b5675ee76611a9ad3b788cc22aa12561c2
comradeskolopendra <comradeskolopendra@gmail.com>
2025-07-12 17:08:55 +0300

    feat: Добавил страницу с отдельным постом

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -7,9 +7,13 @@ interface ArticleProps {
 const Article: FC<ArticleProps> = async ({params}) => {
     const {id} = await params;
 
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const data = await response.json();
+
     return (
         <>
-            {id}
+            <h1>{data.title}</h1>
+            <p>{data.body}</p>
         </>
     )
 };
